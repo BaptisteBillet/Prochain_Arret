1011bef7dfdf7c40e78461586298f3bc75db40ca
daviddelseray <ddelseray@gmail.com>
2015-07-03 17:00:06 +0200

    Amélioration système de navigation

--- Assets/Scripts/Navigationscript.cs
+++ Assets/Scripts/Navigationscript.cs
@@ -6,24 +6,79 @@ public class Navigationscript : MonoBehaviour {
 
 	static public bool gamestart = false;
 
+	static public bool gamepause = false;
+	
+
+	static public int gamecount; 
+
+	public int lastgamecount;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+
 	}
+
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (gamestart) 
+
+
+		if (gamestart == true)
 		{
+			lastgamecount=gamecount;
 			Application.LoadLevel("PostalCardsScreen");
+			gamecount=2;
+
 		}
+
+
+
+
+		if (gamepause==true)
+		{
+			lastgamecount=gamecount;
+			Application.LoadLevel("OptionsScreen");
+			gamecount = 0; 
+		}
+
+
+
+
+
+
+		if (gamecount == 0) 
+		{
+
+			if (gamestart) 
+			{
+				if (lastgamecount == 2) 
+				{
+					Application.LoadLevel ("PostalCardsScreen");
+				}
+
+				if (lastgamecount == 1) 
+				{
+					Application.LoadLevel ("TitleScreen");
+				}
+			}
+
+		}
+
+		
+
 	
 	}
 
-	public void startgame () 
+	public void Startgame() 
 	{
 		gamestart = true;
+	}
+
+	public void Pausegame()
+	{
+		gamepause = true;
+
 	}
 }
