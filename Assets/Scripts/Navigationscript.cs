4495ef5214771e6bd8d5a3b9a21e4d85860f59c2
daviddelseray <ddelseray@gmail.com>
2015-07-03 12:11:48 +0200

    intégration système de menus

---
+++ Assets/Scripts/Navigationscript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigationscript : MonoBehaviour {
+
+
+	public bool gamestart = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (gamestart) 
+		{
+			Application.LoadLevel("ecrancartespostales");
+		}
+	
+	}
+
+	public void startgame () 
+	{
+		gamestart = true;
+	}
+}
