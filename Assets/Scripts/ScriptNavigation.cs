b4a5d9e3faa14d7a971cfffc002075295d2b3d38
daviddelseray <ddelseray@gmail.com>
2015-07-06 11:55:55 +0200

    nettoyage

--- Assets/Scripts/ScriptNavigation.cs
+++ Assets/Scripts/ScriptNavigation.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Navigationscript : MonoBehaviour {
+public class ScriptNavigation : MonoBehaviour {
 
 
 	public GameObject paneloption;
