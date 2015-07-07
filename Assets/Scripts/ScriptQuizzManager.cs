5bead86b22c722d6ae1ccaa96795cf5039d08acc
Baptiste <mail@baptistebillet.com>
2015-07-07 18:29:42 +0200

    Git with erro :lock:

--- Assets/Scripts/ScriptQuizzManager.cs
+++ Assets/Scripts/ScriptQuizzManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ScriptQuizzManager : MonoBehaviour {
 
@@ -12,8 +13,9 @@ public class ScriptQuizzManager : MonoBehaviour {
 			return s_Instance;
 		}
 	}
-	#endregion
+	
 
+	
 
 	void Awake()
 	{
@@ -21,26 +23,133 @@ public class ScriptQuizzManager : MonoBehaviour {
 			s_Instance = this;
 		//DontDestroyOnLoad(this);
 	}
-	
+	#endregion
 
-	// Use this for initialization
-	void Start () 
+	#region Members
+	//Access to Papish
+	public GameObject m_Papish;
+	//Access to the Bubble
+	public GameObject m_Bubble;
+		public Animator m_BubbleAnimator;
+
+	//Creation of a List for random quesions
+	private List<int> m_QuestionList = new List<int>() { 0, 1, 2, 3, 4 };
+	//Keep in memory the Random Number from list
+	private int m_QuestionNumberFromList;
+	
+	//The score
+	private int m_Score;
+
+	//Access to the QuestionBoard
+	public GameObject m_QuestionsBoard;
+
+	//Access to the AnswerBoard
+	public GameObject m_AnswerBoard;
+	public Animator m_AnswerBoardAnimator;
+
+	public GameObject m_ButtonPanel;
+
+
+	//Array for the anwser
+	public int[] m_ArrayOfAnswers = new int[5];
+	#endregion
+
+	void Start()
 	{
-		//Initialisation
-
-
-
+		#region Initialization
+		m_Score = 0;
+		m_QuestionNumberFromList = 0;
+		m_Papish.SetActive(false);
+		m_QuestionsBoard.SetActive(false);
+		m_ButtonPanel.SetActive(false);
+		#endregion
+		
+		StartCoroutine(LaunchPapish());
 	}
-	
-	IEnumerator Openning()
+
+	IEnumerator LaunchPapish()
 	{
+		//On attend que le fond soit apparu
+		yield return new WaitForSeconds(1f);
+		//On lance le mouvement de papish
+		m_Papish.SetActive(true);
+		//On attend la fin du mouvement de papish
+		yield return new WaitForSeconds(1f);
+		//On le fait parler
+		m_Bubble.SetActive(true);
+		yield return new WaitForSeconds(3f);
 
+		StartQuiz();
 
-		yield return null;
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void StartQuiz()
+	{
+		if (m_ButtonPanel.activeInHierarchy == false)
+		{
+			m_ButtonPanel.SetActive(true);
+		}
+		if(m_QuestionsBoard.activeInHierarchy==false)
+		{
+			m_QuestionsBoard.SetActive(true);
+		}
+		
+		m_BubbleAnimator.SetInteger("QuestionNumber", m_QuestionNumberFromList+1);
+		m_BubbleAnimator.SetTrigger("NewQuestion");
+
+
+		if(m_AnswerBoard.activeInHierarchy==false)
+		{
+			m_AnswerBoard.SetActive(true);
+		}
+		
+		m_AnswerBoardAnimator.SetInteger("AnswerNumber", m_QuestionNumberFromList + 1);
+		m_AnswerBoardAnimator.SetTrigger("NewAnswer");
+
+
+		m_QuestionNumberFromList++;
+		//Random
+		/*
+		if(m_QuestionList.Count>0)
+		{
+			m_QuestionNumberFromList = Random.Range(m_QuestionList[0], m_QuestionList.Count);
+			m_BubbleAnimator.SetInteger("QuestionNumber", m_QuestionNumberFromList);
+			m_QuestionList.Remove(m_QuestionNumberFromList);
+			m_BubbleAnimator.SetTrigger("NewQuestion");
+		}
+		else
+		{
+
+		}
+		*/
+	}
+
+	public void Answer(int AnswerNumber)
+	{
+		Debug.Log("a");
+		StartCoroutine(GoodAnswer(AnswerNumber));
+	}
+
+	public IEnumerator GoodAnswer(int AnswerNumber)
+	{
+		Debug.Log("a");
+		if(AnswerNumber==m_ArrayOfAnswers[m_QuestionNumberFromList])
+		{
+			m_BubbleAnimator.SetTrigger("GoodAnswer");
+		}
+		else
+		{
+			m_BubbleAnimator.SetTrigger("BadAnswer");
+		}
+
+		yield return new WaitForSeconds(3);
+		m_BubbleAnimator.SetTrigger("NewQuestion");
+		StartQuiz();
+	}
+
 	
-	}
+
 }
+
+
+
