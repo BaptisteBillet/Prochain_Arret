44c177107b335691751b3178da54a563139465a3
Baptiste <mail@baptistebillet.com>
2015-07-07 13:59:43 +0200

    Start working on the quizz

---
+++ Assets/Scripts/ScriptQuizzManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptQuizzManager : MonoBehaviour {
+
+	#region Singleton
+	static private ScriptQuizzManager s_Instance;
+	static public ScriptQuizzManager instance
+	{
+		get
+		{
+			return s_Instance;
+		}
+	}
+	#endregion
+
+
+	void Awake()
+	{
+		if (s_Instance == null)
+			s_Instance = this;
+		//DontDestroyOnLoad(this);
+	}
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Initialisation
+
+
+
+	}
+	
+	IEnumerator Openning()
+	{
+
+
+		yield return null;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
