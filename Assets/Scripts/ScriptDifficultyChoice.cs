f05378d9d4351c4b2c8b6eafd86ea44ecd90d0e1
Baptiste <mail@baptistebillet.com>
2015-07-08 12:26:43 +0200

    Add Difficulty system and UFO

---
+++ Assets/Scripts/ScriptDifficultyChoice.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptDifficultyChoice : MonoBehaviour {
+
+	void Awake()
+	{
+		switch (Application.platform)
+		{
+			case RuntimePlatform.Android:
+
+				break;
+
+			case RuntimePlatform.WindowsPlayer:
+				PlayerPrefs.SetString("Difficulty", "NULL");
+				break;
+
+			case RuntimePlatform.WindowsEditor:
+				PlayerPrefs.SetString("Difficulty", "NULL");
+				break;
+
+		}
+	}
+
+	public void SaveDifficulty(string difficulty)
+	{
+		switch (Application.platform)
+		{
+			case RuntimePlatform.Android:
+
+				break;
+
+			case RuntimePlatform.WindowsPlayer:
+				PlayerPrefs.SetString("Difficulty",difficulty);
+				break;
+
+			case RuntimePlatform.WindowsEditor:
+				PlayerPrefs.SetString("Difficulty",difficulty);
+				break;
+
+		}
+		Destroy(this.gameObject);
+		
+	}
+
+	/*
+	 IEnumerator WaitForDifficulty()
+	{
+		switch (Application.platform)
+		{
+			case RuntimePlatform.Android:
+
+				break;
+
+			case RuntimePlatform.WindowsPlayer:
+				while(PlayerPrefs.GetString("Difficulty")=="NULL")
+				{
+					yield return new WaitForSeconds(0.5f);
+				}
+				break;
+
+			case RuntimePlatform.WindowsEditor:
+				while (PlayerPrefs.GetString("Difficulty") == "NULL")
+				{
+					yield return new WaitForSeconds(0.5f);
+				}
+				break;
+
+		}
+		//ICI lance l'activité
+	
+	}
+	*/
+
+	/*
+	 
+	switch (Application.platform)
+		{
+			case RuntimePlatform.Android:
+
+				break;
+
+			case RuntimePlatform.WindowsPlayer:
+				switch PlayerPrefs.GetString("Difficulty"):
+					{
+						case "Easy":
+							break;
+						case "Medium":
+							break;
+						case "Hard":
+							break;
+
+			case RuntimePlatform.WindowsEditor:
+				while (PlayerPrefs.GetString("Difficulty") == "NULL")
+				{
+					yield return new WaitForSeconds(0.5f);
+				}
+				break;
+
+		}
+	 */
+}
--- Assets/Scripts/ScriptQuizzManager.cs
+++ Assets/Scripts/ScriptQuizzManager.cs
@@ -30,7 +30,26 @@ public class ScriptQuizzManager : MonoBehaviour {
 	public GameObject m_Papish;
 	//Access to the Bubble
 	public GameObject m_Bubble;
-		public Animator m_BubbleAnimator;
+	//Access to the Bublle Animator
+	public Animator m_BubbleAnimator;
+	//Access to the QuestionBoard
+	public GameObject m_QuestionsBoard;
+
+	//Access to the AnswerBoard
+	public GameObject m_AnswerBoard;
+	//Access to the AnswerBoard Animator
+	public Animator m_AnswerBoardAnimator;
+
+	//Access to the ButtonPanel
+	public GameObject m_ButtonPanel;
+	//Access to the Ufo Animator
+	public Animator m_Ufo1Animator;
+	public Animator m_Ufo2Animator;
+	public Animator m_Ufo3Animator;
+	public Animator m_Ufo4Animator;
+	public Animator m_Ufo5Animator;
+
+
 
 	//Creation of a List for random quesions
 	private List<int> m_QuestionList = new List<int>() { 0, 1, 2, 3, 4 };
@@ -39,16 +58,11 @@ public class ScriptQuizzManager : MonoBehaviour {
 	
 	//The score
 	private int m_Score;
-
-	//Access to the QuestionBoard
-	public GameObject m_QuestionsBoard;
-
-	//Access to the AnswerBoard
-	public GameObject m_AnswerBoard;
-	public Animator m_AnswerBoardAnimator;
-
-	public GameObject m_ButtonPanel;
-
+	//The Objectif
+	private int m_Goal;
+	public int m_GoalEasy;
+	public int m_GoalMedium;
+	public int m_GoalHard;
 
 	//Array for the anwser
 	public int[] m_ArrayOfAnswers = new int[5];
@@ -63,8 +77,35 @@ public class ScriptQuizzManager : MonoBehaviour {
 		m_QuestionsBoard.SetActive(false);
 		m_ButtonPanel.SetActive(false);
 		#endregion
-		
+
+		StartCoroutine(WaitForDifficulty());
+	}
+
+	IEnumerator WaitForDifficulty()
+	{
+		switch (Application.platform)
+		{
+			case RuntimePlatform.Android:
+
+				break;
+
+			case RuntimePlatform.WindowsPlayer:
+				while(PlayerPrefs.GetString("Difficulty")=="NULL")
+				{
+					yield return new WaitForSeconds(0.5f);
+				}
+				break;
+
+			case RuntimePlatform.WindowsEditor:
+				while (PlayerPrefs.GetString("Difficulty") == "NULL")
+				{
+					yield return new WaitForSeconds(0.5f);
+				}
+				break;
+
+		}
 		StartCoroutine(LaunchPapish());
+
 	}
 
 	IEnumerator LaunchPapish()
@@ -77,7 +118,7 @@ public class ScriptQuizzManager : MonoBehaviour {
 		yield return new WaitForSeconds(1f);
 		//On le fait parler
 		m_Bubble.SetActive(true);
-		yield return new WaitForSeconds(3f);
+		yield return new WaitForSeconds(2f);
 		m_BubbleAnimator.SetTrigger("Reset");
 		StartQuiz();
 
@@ -85,6 +126,8 @@ public class ScriptQuizzManager : MonoBehaviour {
 
 	void StartQuiz()
 	{
+		//No Random
+			//We check if they are questions again
 		if (m_QuestionNumberFromList< 5)
 		{
 			if (m_ButtonPanel.activeInHierarchy == false)
@@ -111,9 +154,16 @@ public class ScriptQuizzManager : MonoBehaviour {
 
 			m_QuestionNumberFromList++;
 		}
-		else
+		else //If not, we check if the player win or loose depend of m_Score and m_Objectif
 		{
-			Debug.Log("No More");
+			if (m_Score >= m_Goal)
+			{
+				m_BubbleAnimator.SetTrigger("Win");
+			}
+			else
+			{
+				m_BubbleAnimator.SetTrigger("Loose");
+			}
 		}
 		//Random
 		/*
@@ -142,14 +192,62 @@ public class ScriptQuizzManager : MonoBehaviour {
 		if(AnswerNumber==m_ArrayOfAnswers[m_QuestionNumberFromList-1])
 		{
 			m_BubbleAnimator.SetTrigger("GoodAnswer");
+			m_Score++;
+			switch(m_QuestionNumberFromList)
+			{
+				case 1:
+					m_Ufo1Animator.SetTrigger("Good");
+					break;
+
+				case 2:
+					m_Ufo2Animator.SetTrigger("Good");
+					break;
+
+				case 3:
+					m_Ufo3Animator.SetTrigger("Good");
+					break;
+
+				case 4:
+					m_Ufo4Animator.SetTrigger("Good");
+					break;
+
+				case 5:
+					m_Ufo5Animator.SetTrigger("Good");
+					break;
+
+			}
 		}
 		else
 		{
 			m_BubbleAnimator.SetTrigger("BadAnswer");
+			switch (m_QuestionNumberFromList)
+			{
+				case 1:
+					m_Ufo1Animator.SetTrigger("Bad");
+					break;
+
+				case 2:
+					m_Ufo2Animator.SetTrigger("Bad");
+					break;
+
+				case 3:
+					m_Ufo3Animator.SetTrigger("Bad");
+					break;
+
+				case 4:
+					m_Ufo4Animator.SetTrigger("Bad");
+					break;
+
+				case 5:
+					m_Ufo5Animator.SetTrigger("Bad");
+					break;
+
+			}
 		}
 
 		yield return new WaitForSeconds(3);
 		m_BubbleAnimator.SetTrigger("Reset");
+		yield return new WaitForSeconds(0.1f);
 		StartQuiz();
 	}
 
