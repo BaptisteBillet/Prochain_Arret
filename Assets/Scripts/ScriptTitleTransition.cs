3e2aa4451a76dff6c65c01a3e0a1244a7f479c1d
Baptiste <mail@baptistebillet.com>
2015-07-10 16:52:57 +0200

    Some change to title

---
+++ Assets/Scripts/ScriptTitleTransition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptTitleTransition : MonoBehaviour {
+
+
+	public float m_PlayTime;
+
+	public void Play()
+	{
+		StartCoroutine(CoroutinePlay());
+	}
+
+	IEnumerator CoroutinePlay()
+	{
+		yield return new WaitForSeconds(m_PlayTime);
+		Application.LoadLevel(0);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+}
