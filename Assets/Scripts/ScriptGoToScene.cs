8dd42d58f6364f64c859f4f014abe74a8513559d
Baptiste <mail@baptistebillet.com>
2015-07-10 17:06:23 +0200

    Creation of the application.loadlevel script
    :+1:

---
+++ Assets/Scripts/ScriptGoToScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptGoToScene : MonoBehaviour {
+
+	public void GoToScene(int number)
+	{
+		if(number>-1)
+		{
+			Application.LoadLevel(number);
+		}
+		
+		if(number==-1)
+		{
+			Application.Quit();
+		}
+	}
+}
