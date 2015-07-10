6c1dd4e4c0b00f78d651ca8ef831d43991596dde
Baptiste <mail@baptistebillet.com>
2015-07-10 15:15:32 +0200

    Changement sur l'écran start

---
+++ Assets/Scripts/ScriptAnimationRandom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class ScriptAnimationRandom : MonoBehaviour {
+	
+	private Animator m_Animator;
+	private int m_Random;
+	 [Tooltip("Need to be superior to 0, equal at the maximum number of animation you want to randomize in the animator")]
+	 [Range(1, 99)]
+	public int m_Max;
+	// Use this for initialization
+	void Start () {
+		m_Animator = GetComponent<Animator>();
+	}
+
+	public void RandomAnimation()
+	{
+		if (m_Random==0)
+		{
+			m_Random = Random.Range(1, m_Max + 1);
+		}
+		else
+		{
+			m_Random = 0;
+		}
+		m_Animator.SetInteger("Random", m_Random);
+	}
+}
---
+++ Assets/Scripts/ScriptSimpleRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptSimpleRotation : MonoBehaviour {
+
+	public float m_AxeX;
+	public float m_AxeY;
+	public float m_AxeZ;
+		
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(Vector3.right * Time.deltaTime * m_AxeX);
+		transform.Rotate(Vector3.up * Time.deltaTime * m_AxeY);
+		transform.Rotate(Vector3.forward * Time.deltaTime * m_AxeZ);
+	}
+}
