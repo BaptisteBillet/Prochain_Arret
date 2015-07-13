a828dba60c4dab30efe0dc822d6ffe5f485e2997
daviddelseray <ddelseray@gmail.com>
2015-07-13 11:56:56 +0200

    Papish memory juteux

--- Assets/Scripts/ScriptPapishAnimations.cs
+++ Assets/Scripts/ScriptPapishAnimations.cs
@@ -5,14 +5,39 @@ public class ScriptPapishAnimations : MonoBehaviour {
 
 	int m_RandomChoiceAnim;
 	public float m_MaxAnimNumber;
-	private Animator m_Animator;
+	public Animator m_Animator;
+	float m_ResetTime;
 
+	bool m_TrendingAnimation = false;
 
 	void OnMouseDown ()
 	{
-		m_RandomChoiceAnim = (int) (UnityEngine.Random.Range (0f,m_MaxAnimNumber));
+		if (m_TrendingAnimation == false) 
+		{
+			m_TrendingAnimation = true;
+			m_RandomChoiceAnim = (int)(UnityEngine.Random.Range (0f, m_MaxAnimNumber));
 
-		m_Animator.SetInteger ("ClickedOn", m_RandomChoiceAnim);
+			m_Animator.SetInteger ("ClickedOn", m_RandomChoiceAnim);
+
+			m_Animator.SetTrigger ("NewAnimation");
+
+			StopCoroutine (ResetTrendingAnimation());
+
+			StartCoroutine (ResetTrendingAnimation());
+		} 
+
 
 	}
+
+	IEnumerator ResetTrendingAnimation ()
+	{
+		yield return new WaitForSeconds (1f);
+		m_Animator.ResetTrigger ("NewAnimation");
+		m_Animator.SetInteger ("ClickedOn", -1);
+		m_TrendingAnimation = false;
+
+
+	}
+
+
 }
