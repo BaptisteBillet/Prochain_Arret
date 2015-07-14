b84a2ef7be09556db967c5bd6ebba644a4b188fe
daviddelseray <ddelseray@gmail.com>
2015-07-14 12:31:12 +0200

    misc

--- Assets/Scripts/Visite/ScriptVisiteManager.cs
+++ Assets/Scripts/Visite/ScriptVisiteManager.cs
@@ -5,7 +5,11 @@ public class ScriptVisiteManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+<<<<<<< HEAD
 		ScriptTextSystem.instance.Display2 (2, 0);
+=======
+		ScriptTextSystem.instance.Display1 (2, 0);
+>>>>>>> origin/master
 	}
 	
 
