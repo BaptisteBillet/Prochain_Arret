200e3741d51c6cb0cc8334d776a09cd1f9d61afc
daviddelseray <ddelseray@gmail.com>
2015-07-14 16:00:44 +0200

    Début Visite

--- Assets/Scripts/Visite/ScriptVisiteManager.cs
+++ Assets/Scripts/Visite/ScriptVisiteManager.cs
@@ -3,12 +3,25 @@ using System.Collections;
 
 public class ScriptVisiteManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float m_WaitDepart;
+	public float m_WaitBetween;
 
-		ScriptTextSystem.instance.Display2 (2, 0);
+	public int m_StoryLine;
 
+	void Start () 
+	{
+		StartCoroutine (AttenteDepart());
+
+		ScriptTextSystem.instance.Display1 (0, 0);
+		//StartCoroutine (Attente());// La coroutine n'arrete pas la progression du programme, elle se fait en parallèle. 
+		Debug.Log ("1");
+		ScriptTextSystem.instance.Display1 (1, 0);
 	}
 	
-
+	IEnumerator AttenteDepart ()
+	{
+		Debug.Log ("0");
+		yield return new WaitForSeconds (1f);
+		Debug.Log ("ok");
+	}
 }
