7b6a6c409c63aabeef1f79fc7346c8427fc6f00e
daviddelseray <ddelseray@gmail.com>
2015-07-15 14:06:57 +0200

    Manque encore le son sur la visite mais sinon c'est pas trop mal

--- Assets/Scripts/Visite/ScriptVisiteManager.cs
+++ Assets/Scripts/Visite/ScriptVisiteManager.cs
@@ -5,8 +5,7 @@ public class ScriptVisiteManager : MonoBehaviour {
 
 	public float m_WaitDepart;
 	public float m_WaitBetween;
-
-	public int m_StoryLine;
+	public float m_WaitSupImage;
 
 
 
@@ -23,6 +22,8 @@ public class ScriptVisiteManager : MonoBehaviour {
 
 		ScriptTextSystem.instance.Display1 (2, m_WaitBetween*3);//Ce vol entrera dans l'histoire de Metz. En 1816, une bourgeoise de Metz s'aperçut que ses bijoux lui avaient été dérobés. Furieuse, elle décida de mener son enquête.
 
+		ScriptImageSystem.instance.ImageDisplay (1, m_WaitBetween*3 + m_WaitSupImage);
+
 		ScriptTextSystem.instance.Display1 (3, m_WaitBetween*4);//Elle finit par accuser sa femme de chambre. La malheureuse servante de réussit pas à prouver son innocence. Toute la ville de Metz l’accusa à son tour, et la pauvre bonne fut pendue.
 
 		ScriptTextSystem.instance.Display1 (4, m_WaitBetween*5);//Plus tard, la fille du maire, Mademoiselle de	Turmel, décida d’aller se promener.
@@ -30,6 +31,8 @@ public class ScriptVisiteManager : MonoBehaviour {
 		ScriptTextSystem.instance.Display1 (5, m_WaitBetween*6);//Durant son excursion, elle remarqua une pie qui transportait dans son bec ce qui semblait être un collier.
 
 		ScriptTextSystem.instance.Display1 (6, m_WaitBetween*7);//Elle décida de suivre l’oiseau et, à sa grande surprise, découvrit que son nid était rempli de bijoux, dont ceux de la bourgeoise
+
+		ScriptImageSystem.instance.ImageDisplay (2, m_WaitBetween*7 + m_WaitSupImage);
 
 		ScriptTextSystem.instance.Display2 (1,m_WaitBetween*8);// Ohlalaaaaaaa
 
@@ -40,6 +43,8 @@ public class ScriptVisiteManager : MonoBehaviour {
 		ScriptTextSystem.instance.Display1 (8, m_WaitBetween*10);//Les habitants de Metz eurent honte d’avoir accusé la servante à tort.
 
 		ScriptTextSystem.instance.Display1 (9, m_WaitBetween*11);//Et pour que celle-ci puisse entendre leurs excuses depuis le ciel, Mademoiselle de Turmel fit placer une cloche dans la plus haute tour de la cathédrale.
+
+		ScriptImageSystem.instance.ImageDisplay (3, m_WaitBetween*11 + m_WaitSupImage);
 
 		ScriptTextSystem.instance.Display1 (10, m_WaitBetween*12);//Depuis, la cloche qui porte désormais le nom de Turmel, sonne tous les jours à 21h54. Tendez l’oreille, ce sont des pardons qui montent au ciel…
 	}
