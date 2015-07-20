15d2f348be815f1c52d0eccdf9645d818777155e
Baptiste <mail@baptistebillet.com>
2015-07-20 11:11:09 +0200

    Git git git giiiiiiiiiiit

--- Assets/Scripts/General/ScriptPapishManager.cs
+++ Assets/Scripts/General/ScriptPapishManager.cs
@@ -1,21 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PapishManager : MonoBehaviour {
+public class ScriptPapishManager : MonoBehaviour {
 
-
-
-
+	#region Singleton
+	static private ScriptPapishManager s_Instance;
+	static public ScriptPapishManager instance
+	{
+		get
+		{
+			return s_Instance;
+		}
+	}
+	void Awake()
+	{
+		if (s_Instance == null)
+			s_Instance = this;
+		//DontDestroyOnLoad(this);
+	}
+	#endregion
 
 	public Animator m_Animator;
 
-	// Use this for initialization
-	void Start () {
-	
+	public enum PapishAnimation
+	{
+		IDLE,
+		VICTORY
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void PlayAnimation(PapishAnimation Animation)
+	{
+		switch(Animation)
+		{
+			case PapishAnimation.IDLE:
+				m_Animator.SetTrigger("Idle");	
+				break;
+
+			case PapishAnimation.VICTORY:
+				m_Animator.SetTrigger("Victory");
+				break;
+		}
+		
+
 	}
+
+
+	public void PlaySound(SoundManagerType Sound)
+	{
+		SoundManagerEvent.emit(Sound);
+	}
+
+
 }
