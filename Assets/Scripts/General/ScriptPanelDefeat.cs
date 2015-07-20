8afc8f3531fea15df7e4b5727e22142b772dc895
daviddelseray <ddelseray@gmail.com>
2015-07-20 16:39:54 +0200

    UI sup

---
+++ Assets/Scripts/General/ScriptPanelDefeat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptPanelDefeat : MonoBehaviour {
+
+	public void RestartLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+	
+	public void Return()
+	{
+		Application.LoadLevel ("PostalCardsScreen");
+		
+	}
+}
---
+++ Assets/Scripts/General/ScriptPanelPlayingUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptPanelPlayingUI : MonoBehaviour 
+{
+	public GameObject m_PanelWhirlpool;
+	public GameObject m_PanelPause;
+
+
+	public void Pause ()
+	{
+		m_PanelWhirlpool.SetActive (true);
+		m_PanelPause.SetActive (true);
+	}
+
+}
