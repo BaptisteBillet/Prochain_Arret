3ef9baeb8321a7490393915a75ad5f695798ef4c
daviddelseray <ddelseray@gmail.com>
2015-07-24 15:13:24 +0200

    Shop Improvement

--- Assets/Scripts/Shop/ScriptShopManager.cs
+++ Assets/Scripts/Shop/ScriptShopManager.cs
@@ -15,13 +15,17 @@ public class ScriptShopManager : MonoBehaviour
 	public GameObject m_PanelInterface;
 	public GameObject m_PanelUI;
 
+	public GameObject m_CurrentPanel;
 
+	int m_Price;
 
 	Renderer m_ModelRenderer;
+	MeshFilter m_ModelMesh;
 
 	void Start ()
 	{
 		m_ModelRenderer = m_Model.GetComponent<Renderer>(); 
+		m_ModelMesh = m_Model.GetComponent<MeshFilter>(); 
 		m_Model.SetActive (false);
 	}
 
@@ -29,6 +33,7 @@ public class ScriptShopManager : MonoBehaviour
 
 	public void LoadShopSection (GameObject destination)
 	{
+		m_CurrentPanel = destination;
 		m_PanelSelect.SetActive (false);
 		destination.SetActive (true);
 		m_PanelUI.SetActive (true);
@@ -46,16 +51,42 @@ public class ScriptShopManager : MonoBehaviour
 	
 	}
 
+	public void ChangeModelAspect2 (Mesh newmesh)
+	{
+		m_ModelMesh.mesh = newmesh;
+		
+	}
+
+	public void ChangeModelAspect3 (MeshFilter newmeshfilter)
+	{
+		m_ModelMesh.mesh = newmeshfilter.mesh;
+		
+	}
+
 	public void ModelAspectDescription (string description)
 	{
 		m_Description.text = description;
 	}
 
+	public void StockPrice(int price)
+	{
+		m_Price = price;
+	}
 
+	public void Buy ()
+	{
 
+		Debug.Log ("Item bought for" + m_Price);
+	}
 
+	public void Back()
+	{
+		m_CurrentPanel.SetActive (false);
+		m_PanelUI.SetActive (false);
+		m_Model.SetActive (false);
+		m_PanelSelect.SetActive (true);
 
-
+	}
 
 
 
